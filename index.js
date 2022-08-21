4792f8a6d8b705f8ffb0c710f6bc92c195b0d573
Oscar <sanchez.torres.oscar.a@gmail.com>
2022-08-21 03:25:56 -0500

    feat: Añadido apartado de contacto, footer, imagenes y títulos de las cards. Se modificó el tamaño de la navbar y se hizo colapsable el menú responsive al seleccionar una opción.

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('[
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarColor02')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
